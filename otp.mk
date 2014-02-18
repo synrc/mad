cc67cee7caf496e8618a56ce3d4c3b2d2649090d
Maxim Sokhatsky <maxim@synrc.com>
2014-02-18 07:11:05 +0200

    fix Travis CI

--- otp.mk
+++ otp.mk
@@ -14,7 +14,7 @@ $(PLT_NAME):
 	dialyzer -pa . -pa deps/*/ebin --build_plt --output_plt $(PLT_NAME) --apps $(APPS) || true
 dialyze: compile $(PLT_NAME)
 	dialyzer deps/*/ebin ebin --plt $(PLT_NAME) --no_native -Werror_handling -Wunderspecs -Wrace_conditions
-ct:
+ct: fetch-deps compile
 	rebar ct skip_deps=true verbose=1
 
 .PHONY: fetch-deps compile escript dialyze ct
