3a281e87c809391c552c5721fe4a2877792b2fb5
Maxim Sokhatsky <maxim@synrc.com>
2014-02-18 05:02:34 +0200

    otp.mk

---
+++ otp.mk
@@ -0,0 +1,20 @@
+empty :=
+ROOTS := deps .
+space := $(empty) $(empty)
+comma := $(empty),$(empty)
+ERL_LIBS := $(subst $(space),:,$(ROOTS))
+
+test: eunit ct
+compile: fetch-deps
+fetch-deps compile clean:
+	./mad $@
+escript: compile
+	./build
+$(PLT_NAME):
+	dialyzer -pa . -pa deps/*/ebin --build_plt --output_plt $(PLT_NAME) --apps $(APPS) || true
+dialyze: compile $(PLT_NAME)
+	dialyzer deps/*/ebin ebin --plt $(PLT_NAME) --no_native -Werror_handling -Wunderspecs -Wrace_conditions
+ct:
+	rebar ct skip_deps=true verbose=1
+
+.PHONY: fetch-deps compile escript dialyze ct
