94015b876a31f64afa2996bb2080701d9a43a8b5
Maxim Sokhatsky <maxim@synrc.com>
2014-08-12 22:41:42 +0300

    include observer

--- src/mad_plan.erl
+++ src/mad_plan.erl
@@ -1,12 +1,11 @@
 -module(mad_plan).
 -compile(export_all).
 
-applist() -> [kernel,stdlib,crypto,mnesia,kvs,cowlib,ranch,cowboy,compiler,syntax_tools,erlydtl,gproc,xmerl,n2o,n2o_sample,fs,active,avz,rest].
-
-%    case file:read_file(".applist") of
-%         {ok,Binary} -> parse_applist(binary_to_list(Binary)++"."); 
-%         {error,Reason} -> main([ list_to_atom(filename:basename(App))
-%                             || App <- filelib:wildcard("{apps,deps}/*")  ] -- ['rebar.config']) end.
+applist() -> 
+    case file:read_file(".applist") of
+         {ok,Binary} -> parse_applist(binary_to_list(Binary)++"."); 
+         {error,Reason} -> main([ list_to_atom(filename:basename(App))
+                             || App <- filelib:wildcard("{apps,deps}/*")  ] -- ['rebar.config']) end.
 
 parse_applist(AppList) ->
     {ok,Tokens,_EndLine} = erl_scan:string(AppList),
@@ -17,7 +16,8 @@ parse_applist(AppList) ->
 relconfig(Apps) ->
     LibDirs = [Dir || Dir <- ["apps", "deps"], case file:read_file_info(Dir) of {ok, _} -> true; _ -> false end],
     {sys, [{lib_dirs,LibDirs}, {rel,"node","1",Apps}, {profile, embedded},
-           {boot_rel,"node"}, {app,observer,[{incl_cond,exclude}]} ]}.
+           {boot_rel,"node"}%, {app,observer,[{incl_cond,exclude}]}
+             ]}.
 
 main(AppList) ->
     Relconfig = relconfig(AppList),
