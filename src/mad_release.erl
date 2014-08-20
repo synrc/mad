4dc31a61db6bfc6599981a02bd15b2cd311b161c
Maxim Sokhatsky <maxim@synrc.com>
2014-08-20 04:27:26 +0300

    relx release handling

---
+++ src/mad_release.erl
@@ -0,0 +1,21 @@
+-module(mad_release).
+-compile(export_all).
+
+release(Name,Apps) ->
+    file:write_file("relx.config",list_to_binary(io_lib:format(
+      "{release,{~s,\"1.0.0\"},~p}.\n"
+      "{include_erts,true}.\n"
+      "{extended_start_script,true}.\n"
+      "{generate_start_script,true}.\n"
+      "{sys_config,\"sys.config\"}.\n"
+      "{overlay,[{mkdir,\"log/sasl\"}]}.\n",[Name,Apps]))).
+
+main([]) -> main(["sample"]);
+main(Params) ->
+    [N|_] = Params,
+    Apps = [ filename:basename(F) || F <- filelib:wildcard("{apps,deps}/*"),  filelib:is_dir(F)],
+    release(N,Apps),
+    {Res,Status,X} = sh:run("relx",[],binary,".",[]),
+    case Status == 0 of
+         true -> skip;
+         false -> io:format("Shell Error: ~s~n\r",[binary_to_list(X)]) end.
