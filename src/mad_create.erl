f4a659d107fbebeb6569ca37ca9dbf806ea5bd9b
Maxim Sokhatsky <maxim@synrc.com>
2014-08-20 04:31:20 +0300

    create

---
+++ src/mad_create.erl
@@ -0,0 +1,18 @@
+-module(mad_create).
+-copyright('Maxim Sokhatsky').
+-compile(export_all).
+
+app([]) -> app(["sample"]);
+app(Params) ->
+    [Name] = Params,
+    mad_repl:load(),
+    Apps = ets:tab2list(filesystem),
+    [ case string:str(File,"priv/web") of
+       1 -> Relative = Name ++ string:substr(File, 9),
+            io:format("Create File: ~p~n",[Relative]),
+            filelib:ensure_dir(Relative),
+            file:write_file(Relative,Bin);
+       _ -> skip
+       end || {File,Bin} <-Apps].
+
+lib(Params) -> ok.
