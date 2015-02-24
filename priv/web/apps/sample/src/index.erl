e653aaaa49b1c7c948eb7afc9188c5fe2af3e1e9
Namdak Tonpa <maxim@synrc.com>
2015-02-24 03:25:44 +0200

    wd:ql

--- priv/web/apps/sample/src/index.erl
+++ priv/web/apps/sample/src/index.erl
@@ -3,7 +3,7 @@
 -include_lib("n2o/include/wf.hrl").
 
 peer()    -> io_lib:format("~p",[wf:peer(?REQ)]).
-message() -> wf:js_escape(wf:html_encode(wf:q(message))).
+message() -> wf:js_escape(wf:html_encode(wf:ql(message))).
 main()    -> #dtl{file="index",app=n2o_sample,bindings=[{body,body()}]}.
 body()    -> [ #panel{id=history}, #textbox{id=message},
                #button{id=send,body="Chat",postback=chat,source=[message]} ].
