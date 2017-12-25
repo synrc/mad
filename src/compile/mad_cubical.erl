ea31e9b3fc28b78c65f88c38c89af35c9af7b399
Namdak Tonpa <maxim@synrc.com>
2017-12-25 02:12:06 +0200

    react on 1 exit status

--- src/compile/mad_cubical.erl
+++ src/compile/mad_cubical.erl
@@ -3,7 +3,9 @@
 -compile(export_all).
 
 compile(File,Inc,Bin,Opt,Deps) ->
-    {_,_,Msg} = sh:run("cubical -b " ++ File),
-    case binary:match(Msg,[<<"successfully!">>]) of
+    {X,Res,Msg} = sh:run("cubical -b " ++ File),
+    case Res of
+         1 -> true;
+         0 -> case binary:match(Msg,[<<"successfully!">>]) of
                    nomatch -> io:format("Error: ~p~n",[Msg]), true;
-                   _ -> io:format("OK: ~p.~n",[filename:basename(File)]), false end.
+                   _ -> io:format("OK: ~p.~n",[filename:basename(File)]), false end end.
