4688037c19a141c0ae9f78f26ed8123e76db8c2f
Namdak Tonpa <maxim@synrc.com>
2018-11-13 10:58:04 +0200

    man

--- include/api.hrl
+++ include/api.hrl
@@ -1,3 +1,22 @@
 -define(MAD,[compile/1,app/1,get/1,release/1,resolve/1,clean/1,
              start/1,attach/1,stop/1,sh/1,deps/1,up/1,fetch/1,
             static/1,eunit/1,strip/1]).
+
+-type return() :: [] | true | false | {ok,any()} | {error,any()}.
+
+-spec compile(list(string())) -> return().
+-spec app(list(string())) -> return().
+-spec get(list(string())) -> return().
+-spec release(list(string())) -> return().
+-spec resolve(list(string())) -> return().
+-spec clean(list(string())) -> return().
+-spec start(list(string())) -> return().
+-spec attach(list(string())) -> return().
+-spec stop(list(string())) -> return().
+-spec sh(list(string())) -> return().
+-spec deps(list(string())) -> return().
+-spec up(list(string())) -> return().
+-spec fetch(list(string())) -> return().
+-spec static(list(string())) -> return().
+-spec eunit(list(string())) -> return().
+-spec strip(list(string())) -> return().
