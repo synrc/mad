ef508478bb71cbf7d39aa0fa276cfd603895459d
Namdak Tonpa <maxim@synrc.com>
2018-11-14 20:37:29 +0200

    local profile

--- src/mad_local.erl
+++ src/mad_local.erl
@@ -4,6 +4,7 @@
 
 compile(Params)   -> mad_compile:compile(Params).
 app(Params)       -> mad_static:app(Params).
+static(Params)    -> mad_static:main([],Params).
 release(Params)   -> mad_release:release(Params).
 strip(Params)     -> mad_release:strip(Params).
 resolve(Params)   -> mad_release:resolve(Params).
@@ -15,6 +16,5 @@ get(Params)       -> mad_git:get_repo(Params).
 deps(Params)      -> mad_git:deps(Params).
 up(Params)        -> mad_git:up(Params).
 fetch(Params)     -> mad_git:fetch(Params).
-static(Params)    -> mad_static:main([],Params).
 eunit(Params)     -> mad_eunit:main_test(Params).
 sh(Params)        -> mad_repl:sh(Params).
