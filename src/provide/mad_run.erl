099842a63ac95f6f1afbe84150cfa931180fc0ce
Vlad Ki <vlad@kirillov.im>
2019-03-06 12:28:56 +0200

    implement mad_run:stop/1, close #89

--- src/provide/mad_run.erl
+++ src/provide/mad_run.erl
@@ -14,7 +14,7 @@ start(App) ->                            % run_dir > < log_dir
 
 attach(_) -> mad:info("to_erl .~n"), {ok,[]}. % use like $(mad attach)
 
-stop(_) -> {error,"Not Implemented."}.
+stop(_) -> mad:info("echo 'init:stop().' | to_erl .~n"), {ok,[]}. % use like $(mad stop)
 
 clean(_) -> [ file:delete(X) || X <- filelib:wildcard("{apps,deps}/*/ebin/*.beam") ++
                                      filelib:wildcard("ebin/*.beam")], {ok,[]}.
