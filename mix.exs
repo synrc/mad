d2e6bc34e758f884d6324001784660ff7254a73b
Namdak Tonpa <maxim@synrc.com>
2019-10-27 19:44:36 +0200

    mix.exs

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule MAD.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :mad,
+      version: "5.10.0",
+      description: "MAD Manage Dependencies",
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  def application do
+     []
+  end
+
+  def package do
+    [
+      files: ~w(include src priv mix.exs rebar.config),
+      licenses: ["ISC"],
+      maintainers: ["Namdak Tonpa"],
+      name: :mad,
+      links: %{"GitHub" => "https://github.com/synrc/mad"}
+    ]
+  end
+
+  def deps do
+    [
+      {:ex_doc, "~> 0.11", only: :dev},
+      {:exe, "~> 3.9.0"}
+    ]
+  end
+end
